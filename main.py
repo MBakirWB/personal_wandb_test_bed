7869ed0664ee7133d258533c88ecaf7d733bf546
MBakirWB <mohammad.bakir@wandb.com>
2024-08-06 18:28:59 -0700

    updated

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import random
 
 WANDB_ENTITY="mb-smle"
 WANDB_PROJECT="git-testing"
-with wandb.init(entity=WANDB_ENTITY, project=WANDB_PROJECT) as run:
-    # Simulate logging model metrics
+with wandb.init(entity=WANDB_ENTITY, project=WANDB_PROJECT, settings = wandb.Settings(disable_git=True, disable_code=True)) as run:
     offset = random.random() / 5
     for epoch in range(1,20):
         acc = 1 - 2 ** -epoch - random.random() / epoch - offset
         loss = 2 ** -epoch + random.random() / epoch + offset
-        t_loss = 2 ** -epoch + random.random() / epoch + offset
-        t_loss_1 = 2 ** -epoch + random.random() / epoch + offset
-        wandb.log({"acc": acc, "loss": loss, "t_loss":t_loss, "t_loss_1":t_loss_1})
+        wandb.log({"acc": acc, "loss": loss})
