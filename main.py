a3099b35a0e5cb1e598e2d325304023041c0336f
MBakirWB <mohammad.bakir@wandb.com>
2024-08-07 00:27:52 -0700

    updates

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import random
 
 WANDB_ENTITY="mb-smle"
 WANDB_PROJECT="git-testing"
-with wandb.init(entity=WANDB_ENTITY, project=WANDB_PROJECT, settings = wandb.Settings(disable_git=True, disable_code=True)) as run:
+with wandb.init(entity=WANDB_ENTITY, project=WANDB_PROJECT) as run:
     offset = random.random() / 5
     for epoch in range(1,20):
         acc = 1 - 2 ** -epoch - random.random() / epoch - offset
         loss = 2 ** -epoch + random.random() / epoch + offset
-        wandb.log({"acc": acc, "loss": loss})
+        t_loss = 2 ** -epoch + random.random() / epoch + offset
+        wandb.log({"acc": acc, "loss": loss, "t_loss":t_loss})
